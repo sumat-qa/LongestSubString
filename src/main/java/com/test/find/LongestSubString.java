8da3fee048b0c17b4b283ee9b1a38528ef8e3fe2
josdem <joseluis.delacruz@gmail.com>
2023-05-15 11:05:38 -0400

    Updating Gradle and Junit, formatting code

--- src/main/java/com/test/find/LongestSubString.java
+++ src/main/java/com/test/find/LongestSubString.java
@@ -1,14 +1,12 @@
 package com.test.find;
 
-import java.util.List;
-
 /**
  * Find the length of the longest substring for the provided string
  * If input is abcabcbb then the longest substring is abc and the length of that would be 3
  */
 
 public class LongestSubString {
-    public int longestSubString(String input){
+    public int longestSubString(String input) {
         int lengthOflongestSubstring=0;
         //Put your code here and return expected results
 
